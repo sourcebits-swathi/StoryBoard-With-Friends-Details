f2733c962a852f39c536c7dec434561c99ced36e
Sourcebits <swathi.tata@sourcebits.com>
2014-05-05 14:48:08 +0530

    Initial Commit

---
+++ FriendDetails/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FriendDetails
+//
+//  Created by Swathi Tata on 05/05/14.
+//  Copyright (c) 2014 Swathi Tata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FriendDetails/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FriendDetails
+//
+//  Created by Swathi Tata on 05/05/14.
+//  Copyright (c) 2014 Swathi Tata. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
